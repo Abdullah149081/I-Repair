881d020a4e8d30f2b11bb0085fb7727e918a7d4e
Abdullah149081 <masudmd425@gmail.com>
2024-01-30 12:45:14 +0600

    Add admin routes to Routes.tsx

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,8 +1,12 @@
-import { createBrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import About from '@/Pages/About';
 import Home from '@/Pages/Home';
 import Services from '@/Pages/Services';
+import DashBoard from '@/Pages/admin/DashBoard';
+import ServicesAdd from '@/Pages/admin/ServicesAdd';
+import ServicesList from '@/Pages/admin/ServicesList';
+import DashBoardLayout from '@/components/layout/DashBoardLayout';
+import { Navigate, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
@@ -20,6 +24,28 @@ const router = createBrowserRouter([
       {
         path: '/services',
         element: <Services />,
+      },
+    ],
+  },
+  {
+    path: '/admin',
+    element: <DashBoardLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/admin/dashBoard" />,
+      },
+      {
+        path: 'dashBoard',
+        element: <DashBoard />,
+      },
+      {
+        path: 'services-list',
+        element: <ServicesList />,
+      },
+      {
+        path: 'services-add',
+        element: <ServicesAdd />,
       },
     ],
   },
