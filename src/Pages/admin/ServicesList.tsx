fd6510bf7c3a9aa8c9ececd904525ed29c515dfd
Abdullah149081 <masudmd425@gmail.com>
2024-01-31 20:15:31 +0600

    Add ServicesList component

---
+++ src/Pages/admin/ServicesList.tsx
@@ -0,0 +1,58 @@
+import { Trash2 } from 'lucide-react';
+import Container from '@/components/ui/Container';
+import { Button } from '@/components/ui/button';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import useServices from '@/hooks/useServices';
+
+const ServicesList = () => {
+  const { data: services, isLoading, isError } = useServices();
+
+  if (isLoading) return <h1>Loading...</h1>;
+
+  if (isError) return <h1>Error</h1>;
+
+  return (
+    <Container className="mt-4">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {services.map((service) => (
+            <TableRow key={service._id}>
+              <TableCell className="font-medium">{service.name}</TableCell>
+              <TableCell>{service.description}</TableCell>
+              <TableCell>{service.price}</TableCell>
+              <TableCell className="text-right">
+                <Button variant="destructive">
+                  <Trash2 className="size-5" />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total Services</TableCell>
+            <TableCell className="text-right">{services.length}</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </Container>
+  );
+};
+
+export default ServicesList;
